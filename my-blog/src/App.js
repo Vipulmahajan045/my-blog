51777fd11dde2a6e2c48e11a4ff1beeab001de11
Vipulmahajan045 <vipulmahajan170140111045@gmail.com>
2021-10-30 23:15:34 +0530

    navbar done

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,11 +1,26 @@
 
-import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+//Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import ArticleList from './pages/ArticleList';
+import Article from './pages/Article';
+
+//Components
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div className="container bg-blue-500 text-center p-5">
-      App components
+    <Router >
+      <Navbar />
+    <div className="max-w-screen-md mx-auto pt-20">
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/article-list' component={ArticleList} />
+      <Route exact path='/article' component={Article} />
     </div>
+    </Router>
   );
 }
 
---
+++ my-blog/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">About</h1>
+      <p className="mx-auto leading-relax text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic debitis nesciunt veritatis corporis at consequuntur accusamus incidunt officia officiis blanditiis, ad voluptatum consectetur quia quidem ex, vel error enim laboriosam accusantium! Placeat recusandae accusamus itaque nisi libero omnis a, quasi excepturi assumenda error. Optio cum porro veniam commodi molestias ab.</p>
+      <p className="mx-auto leading-relax text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic debitis nesciunt veritatis corporis at consequuntur accusamus incidunt officia officiis blanditiis, ad voluptatum consectetur quia quidem ex, vel error enim laboriosam accusantium! Placeat recusandae accusamus itaque nisi libero omnis a, quasi excepturi assumenda error. Optio cum porro veniam commodi molestias ab.</p>
+      <p className="mx-auto leading-relax text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic debitis nesciunt veritatis corporis at consequuntur accusamus incidunt officia officiis blanditiis, ad voluptatum consectetur quia quidem ex, vel error enim laboriosam accusantium! Placeat recusandae accusamus itaque nisi libero omnis a, quasi excepturi assumenda error. Optio cum porro veniam commodi molestias ab.</p>
+    </div>
+  )
+}
+
+export default About

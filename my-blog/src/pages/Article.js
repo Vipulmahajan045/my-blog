6698f0f561d51b042aae85904bb1e0f1aced45b3
Vipulmahajan045 <vipulmahajan170140111045@gmail.com>
2021-11-01 18:24:07 +0530

    changed

--- my-blog/src/pages/Article.js
+++ my-blog/src/pages/Article.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Article = () => {
+const Article = ({match}) => {
+  const name  = match.params.name;
   return (
     <>
-      <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">This is an Article</h1>
+      <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">This is  this {name} Article</h1>
     </>
   )
 }

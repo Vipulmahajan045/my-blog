4444badce08e97d1e463ccf516a5db40ddcd6b98
Vipulmahajan045 <vipulmahajan170140111045@gmail.com>
2021-11-02 14:28:29 +0530

    minor changes

--- my-blog/src/pages/article-content.js
+++ my-blog/src/pages/article-content.js
@@ -1,26 +1,53 @@
-const article  = [
+const articles = [
     {
-        name : "learn-react",
-        title: "The fastest way to learn react",
-        content: [
-            `welcome , to learn react-js cnjhv bcdhgfiw jhjkwhfi oerifor ioier cbnxb zxz sds ere ty yiu jvkmh  goingon jhgkhgc .p,obj bkgjbkg  fjghfksd bvjfhjksvhf jvhfjkgh hvfdjkshvjbjvksf hgfjkhsg nbvjhvsjkf jvbsh`,
-            `i am vipul mahajan my name is good boyyou are a bad boy i know nall about hta so soryy for that i am really soryybrother  xjhsjchgd cjdgjkcds vbdvhjd  hkgjh hjghktjkgj gnjh hfd ghjsfk jhgfkushgis  fsbgjhfk hjfhgsi bjhfjkshgksf hfjhsgi bvfjhs`
-            `Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repellat culpa perspiciatis architecto neque exercitationem accusantium cupiditate quia dicta ipsam quod corrupti modi voluptatibus, quibusdam provident mollitia suscipit, enim error explicabo voluptatem sed molestiae dolorem voluptatum iure. Accusantium debitis ad labore, non temporibus tempore voluptatibus? Distinctio molestiae quos minima laudantium modi aliquid sapiente dolorem, iusto pariatur excepturi nihil unde odio fugit est enim iure, numquam exercitationem recusandae minus nisi tenetur, fuga maxime aut? Ullam ipsa odit cupiditate exercitationem quia nobis quo debitis laudantium ratione nihil dolore, quisquam dicta magnam rerum ab expedita natus, numquam quod earum eaque corporis, molestias voluptatibus.`,
-        ],
+      name: "learn-react",
+      title: "The Fastest Way to Learn React",
+      thumbnail: "/images/blog1.jpg",
+      content: [
+        `Welcome! Today we're going to be talking about the fastest way to learn React. Lorem proin congue ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at sodales purus euismod.`,
+        `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+        `Etiam nec lectus urna. Sed sodales ultrices dapibus. Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+      ],
     },
     {
-        name : "learn-JS",
-        title: "The fastest way to learn JS",
-        content: [
-            `i am good boy you are a bad boy so please lets me know whos is better than me and who is good in knowledge in me bjdfgf fghjsgfus gdhsagfj gdshagvjkdas nbvfsbgjvkf nvbgvjhgf`
-        ],
+      name: "learn-node",
+      title: "How to Build a Node Server in 10 Minutes",
+      thumbnail: "/images/blog2.jpg",
+      content: [
+        `In this article, we're going to be talking looking at a very quick way to set up a Node.js server. We'll be discussing some topics such as proin congue ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at sodales purus euismod.`,
+        `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+        `Etiam nec lectus urna. Sed sodales ultrices dapibus. Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+      ],
     },
     {
-        name : "learn-HTML CSS",
-        title: "The fastest way to learn HTML CSS",
-        content: [
-            `sofpdf fn dxgfhbxg xghfwbgfzqe ggq ufgieq hxhgni hicguiocygu gjfhgjhfgh gfjghf gfgf gh  ju ki k i ku  g g r fe d xfx fd x gx b fx bc nf c n cm vvnhvmnvm  vmjmj b mjb mj b m`
-        ],
+      name: "my-thoughts-on-learning-react",
+      title: "My Thoughts on Learning ReactJS",
+      thumbnail: "/images/blog3.jpg",
+      content: [
+        `Today is the day I talk about something which scares most people: Learning ReactJS. In reality, I'm not sure why people have such a hard time with Learning ReactJS ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at sodales purus euismod.`,
+        `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+        `Etiam nec lectus urna. Sed sodales ultrices dapibus. Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+      ],
     },
-];
-export default article; 
+    {
+        name: "html",
+        title: "Learn HTML",
+        thumbnail: "/images/blog4.jpg",
+        content: [
+          `Today is the day I talk about something which scares most people: Learning ReactJS. In reality, I'm not sure why people have such a hard time with Learning ReactJS ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at sodales purus euismod.`,
+          `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+          `Etiam nec lectus urna. Sed sodales ultrices dapibus. Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+        ],
+      },
+      {
+        name: "css",
+        title: "Learn CSS",
+        thumbnail: "/images/blog5.jpg",
+        content: [
+          `Today is the day I talk about something which scares most people: Learning ReactJS. In reality, I'm not sure why people have such a hard time with Learning ReactJS ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at sodales purus euismod.`,
+          `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+          `Etiam nec lectus urna. Sed sodales ultrices dapibus. Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+        ],
+      },
+  ];
+  export default articles;
